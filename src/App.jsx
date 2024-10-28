2f578ead15c32600c492f57db7343bd499b2fd36
unknown <chandanpradhan414@gmail.com>
2024-10-28 12:40:07 +0530

    It's my Chatapp project initilize commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+const App = ()=>{
+    return(
+        <div className="bg-red-800 min-h-screen">
+            <h1 className="bg-black">hello world</h1>
+            <h2>hello world</h2>
+            <h3>hello world</h3>
+            <h4>hello world</h4>
+            <h5>hello world</h5>
+            <h6>hello world</h6>
+        </div>
+    )
+}
+
+export default App;
